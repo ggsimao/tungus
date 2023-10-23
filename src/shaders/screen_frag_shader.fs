998d5582e5491aac36cb0e60345694b50b8a1947
ggsimao <cd_giulio@hotmail.com>
2023-10-23 00:02:50 -0300

    Added convolution to the screen shader

--- src/shaders/screen_frag_shader.fs
+++ src/shaders/screen_frag_shader.fs
@@ -5,6 +5,18 @@ out vec4 fragColor;
 
 uniform sampler2D screenTexture;
 
+const float offset = 1.0 / 300.0;
+
+const float kernel[3][3] = float[][](
+    float[](2,2,2),
+    float[](2,-15,2),
+    float[](2,2,2));
+
 void main() {
-    fragColor = texture(screenTexture, texCoords);
+    fragColor = vec4(0);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            fragColor += texture(screenTexture, texCoords.st + vec2(i-1, j-1) * offset) * kernel[i][j];
+        }
+    }
 }
